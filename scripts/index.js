6182aeb6345a9d30b55efc1487422594031364b1
Fran Aguirre <franaguirre.alt@gmail.com>
2023-09-29 11:54:25 -0300

    Añadiendo más musica a la Aplicación

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,50 @@
 const d = document;
 
-const $playlistSong = d.querySelector(".playlist__song-container:first-child");
-const $song = d.querySelector("audio");
+const $playlist = d.querySelector(".playlist");
+
+const $playlistSong = d.querySelectorAll(".playlist__song-container");
+const $p = [...d.querySelectorAll(".playlist__song-container > p")];
+// const $p = d.querySelectorAll(".playlist__song-container > p");
+
 const $artistSongTitle = d.querySelector(".head__artist-song-title > p");
 
-$playlistSong.addEventListener("click", () => {
+/* const $playlistSong = d.querySelector(".playlist__song-container:first-child");
+const $song = d.querySelector("audio");
+const $artistSongTitle = d.querySelector(".head__artist-song-title > p"); */
+
+
+
+const debug = (param) => console.log(...param);
+debug($p);
+
+const OBJ = { nombre: $p
+}
+
+console.log(OBJ.nombre[1])
+
+// console.log($p.map)
+
+
+$playlist.addEventListener("click", (e) => {
+    const $2 = $p[2];
+
+    // console.log($playlistSong);
+    // console.log($4);
+    // console.log($p);
+
+
+    if (e.target === $2) {
+        // console.log("Hiiii");
+        $artistSongTitle.textContent = $2.textContent;
+    }
+
+    // if ($playlistSong) {
+    //     $artistSongTitle.textContent = $p.textContent;
+    // }
+    // console.log(e.target);
+})
+
+/* $playlistSong.addEventListener("click", () => {
     $artistSongTitle.textContent = $playlistSong.textContent;
     $song.play();
-});
+}); */
