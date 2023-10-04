6e05050674087f1d0c37aed209ad60698fb6989c
Fran Aguirre <franaguirre.alt@gmail.com>
2023-10-04 12:51:38 -0300

    Depurando un poco más el código de index.js

--- scripts/index.js
+++ scripts/index.js
@@ -20,9 +20,6 @@ $playlist.addEventListener("click", (e) => {
             $playlistSong[i].classList.add("active");     
             $playlistSongName[i].classList.add("active__name");
 
-            // $song[i].play(); 
-
-
             //Este código sirve para tener el tiempo actual de la canción (Implementar más adelante)
             // const h = $song[i].currentTime;
             // console.log(h);  
@@ -34,37 +31,6 @@ $playlist.addEventListener("click", (e) => {
             $playlistSongName[i].classList.remove("active__name"); 
             $audioActual[i].pause();
             $audioActual[i].removeAttribute("src");
-            // audioActual.removeAttribute("src");
         }
     }   
 })
-
-
-/* $playlist.addEventListener("click", (e) => {
-    for (let i = 0; i < $playlistSong.length; i++) {
-        if(e.target === $playlistSong[i]){
-            console.log("Haz apretado el array " + i)
-            $playlistSong[i].classList.add('active');
-        }
-    }
-
-    if(e.target.textContent) {
-        console.log(e);
-        const contenedorMusica = e.target.textContent;
-        e.target.parentElement.classList.add('active');
-        $artistSongTitle.textContent = e.target.textContent;
-        const $song = d.createElement("audio");
-        $song.src=`../songs/${contenedorMusica}.mp3`
-        $song.play();
-    }
-}) */
-
-// console.log($playlistSongPrueba[10])
-// console.log($playlistSong[10])
-
-// const almacenamiento = $playlistSongPrueba.map((el) => 
-//     {
-//         myArray.push(el);
-//     }
-// );
-// console.log(almacenamiento);
