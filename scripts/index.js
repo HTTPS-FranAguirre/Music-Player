6c6a128d4114d2efeec255198e14b713a624f515
Fran Aguirre <franaguirre.alt@gmail.com>
2023-10-05 14:43:16 -0300

    Añadiendo duración de la canción

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 const d = document;
 
+const $headCurrentSongTime = d.querySelector(".head__current-song-time");
+
 const $playlist = d.querySelector(".playlist");
 
 const $playlistSong = d.querySelectorAll(".playlist__song-container");
@@ -7,16 +9,26 @@ const $playlistSongName = d.querySelectorAll(".playlist__song-container p");
 
 const $artistSongTitle = d.querySelector(".head__artist-song-title p");
 
-const $audioActual= d.querySelectorAll("audio");
+const $song= d.querySelectorAll("audio");
 
 $playlist.addEventListener("click", (e) => {
     for (let i = 0; i < $playlistSongName.length; i++) {
 
          if(e.target === $playlistSong[i] || e.target === $playlistSongName[i]) {
             $artistSongTitle.textContent = $playlistSong[i].textContent;
-            $audioActual[i].setAttribute("src", `../songs/${$playlistSongName[i].textContent}.mp3`);
+            $song[i].setAttribute("src", `../songs/${$playlistSongName[i].textContent}.mp3`);
+            
+            $song[i].play();
 
-            $audioActual[i].play();
+            setTimeout(() => {
+                let minutos = Math.floor($song[i].duration / 60);
+                let segundos = Math.floor($song[i].duration % 60);
+                let duracionTotal = `${minutos.toFixed(0)} : ${segundos.toFixed(0)}`
+                const $parrafo = d.createElement("p");
+                $parrafo.textContent = duracionTotal
+                $headCurrentSongTime.appendChild($parrafo);
+              }, 150);
+            
 
             $playlistSong[i].classList.add("active");     
             $playlistSongName[i].classList.add("active__name");
@@ -25,12 +37,12 @@ $playlist.addEventListener("click", (e) => {
             // const h = $song[i].currentTime;
             // console.log(h);  
             //Este código sirve para tener la duracion de la canción (Implementar más adelante)
-            // const g = $song[i].duration;
+            // const g = $song.duration;
             // console.log(g);  
         } else {
             $playlistSong[i].classList.remove("active");
             $playlistSongName[i].classList.remove("active__name"); 
-            $audioActual[i].pause();
+            $song[i].pause();
             // $audioActual[i].removeAttribute("src");
         }
     }   
